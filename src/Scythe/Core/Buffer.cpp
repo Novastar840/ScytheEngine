233b9ced7f43e8bc921bae182ef212ff5114db42
Liam Nagels <Liam.Nagels@outlook.be>
2026-07-09 21:38:24 +0200

    Implemented the opengl buffers

---
+++ src/Scythe/Core/Buffer.cpp
@@ -0,0 +1,28 @@
+﻿#include "Buffer.h"
+#include "RendererAPI.h"
+#include "../Platform/OpenGL/OpenGLBuffer.h"
+
+namespace Scythe
+{
+    std::shared_ptr<VertexBuffer> VertexBuffer::Create(uint32_t size)
+    {
+        switch (RendererAPI::GetAPI())
+        {
+            case RendererAPI::API::None: return nullptr;
+            case RendererAPI::API::OpenGL: return std::make_shared<OpenGLVertexBuffer>(size);
+            case RendererAPI::API::Vulkan: return nullptr;
+        }
+        return nullptr;
+    }
+
+    std::shared_ptr<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t count)
+    {
+        switch (RendererAPI::GetAPI())
+        {
+        case RendererAPI::API::None: return nullptr;
+        case RendererAPI::API::OpenGL: return std::make_shared<OpenGLIndexBuffer>(indices, count);
+        case RendererAPI::API::Vulkan: return nullptr;
+        }
+        return nullptr;
+    }
+}
