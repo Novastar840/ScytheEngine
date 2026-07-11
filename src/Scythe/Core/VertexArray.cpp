416689979922a54dd5425e2561f0049e5375e5cb
Liam Nagels <Liam.Nagels@outlook.be>
2026-07-11 16:17:24 +0200

    Added vertex array abstraction and implemented OpenGL for it

---
+++ src/Scythe/Core/VertexArray.cpp
@@ -0,0 +1,16 @@
+﻿#include "VertexArray.h"
+#include "RendererAPI.h"
+#include "../Platform/OpenGL/OpenGLVertexArray.h"
+
+namespace Scythe
+{
+    std::shared_ptr<VertexArray> VertexArray::Create()
+    {
+        switch (RendererAPI::GetAPI()) {
+        case RendererAPI::API::None:    return nullptr;
+        case RendererAPI::API::OpenGL:  return std::make_shared<OpenGLVertexArray>();
+        case RendererAPI::API::Vulkan:  return nullptr;
+        }
+        return nullptr;
+    }
+}
