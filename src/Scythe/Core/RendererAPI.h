665863e1546d9eb0db0a03d9a162e9e8ed2707e7
Liam Nagels <Liam.Nagels@outlook.be>
2026-07-14 00:57:00 +0200

    Comment clean up

--- src/Scythe/Core/RendererAPI.h
+++ src/Scythe/Core/RendererAPI.h
@@ -5,7 +5,7 @@
 
 namespace Scythe
 {
-    class VertexArray; // Forward declaration for later
+    class VertexArray;
 
     class RendererAPI
     {
@@ -19,24 +19,19 @@ namespace Scythe
 
         virtual ~RendererAPI() = default;
 
-        // Core lifecycle and state commands
         virtual void Init() = 0;
         virtual void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) = 0;
         virtual void SetClearColor(const glm::vec4& color) = 0;
         virtual void Clear() = 0;
 
-        // Draw commands (VertexArray will be built in the next step)
         virtual void DrawIndexed(const std::shared_ptr<VertexArray>& vertexArray, uint32_t indexCount = 0) = 0;
 
-        // The Factory Method
         static std::unique_ptr<RendererAPI> Create();
 
-        // Global API State Accessors
         static API GetAPI() { return s_API; }
         static void SetAPI(API api) { s_API = api; }
 
     private:
-        // Defaults to OpenGL for now
         static API s_API;
     };
 }
