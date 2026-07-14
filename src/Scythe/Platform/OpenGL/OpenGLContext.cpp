c07591309d62cfe2d15852ca737ee5b46f489c32
Liam Nagels <Liam.Nagels@outlook.be>
2026-07-15 00:01:52 +0200

    Added openGL graphics context implementation to the graphics api interface

---
+++ src/Scythe/Platform/OpenGL/OpenGLContext.cpp
@@ -0,0 +1,32 @@
+﻿#include "OpenGLContext.h"
+
+#include <glad/gl.h>
+#include "GLFW/glfw3.h"
+#include <spdlog/spdlog.h>
+
+namespace Scythe
+{
+    OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
+        : m_WindowHandle(windowHandle) {}
+
+    void OpenGLContext::Init()
+    {
+        glfwMakeContextCurrent(m_WindowHandle);
+        int version = gladLoadGL(glfwGetProcAddress);
+        if (version == 0) {
+            spdlog::critical("Failed to initialize GLAD");
+            return;
+        }
+        spdlog::info("OpenGL version {0}", version);
+    }
+
+    void OpenGLContext::SwapBuffers()
+    {
+        glfwSwapBuffers(m_WindowHandle);
+    }
+
+    void OpenGLContext::Resize(int width, int height)
+    {
+        glViewport(0, 0, width, height);
+    }
+}
---
+++ src/Scythe/Platform/OpenGL/OpenGLContext.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "../../Core/GraphicsContext.h"
+
+struct GLFWwindow;
+
+namespace Scythe
+{
+    class OpenGLContext : public GraphicsContext
+    {
+    public:
+        OpenGLContext(GLFWwindow* windowHandle);
+        ~OpenGLContext() override = default;
+        
+        void Init() override;
+        void SwapBuffers() override;
+        void Resize(int width, int height) override;
+    private:
+        GLFWwindow* m_WindowHandle;
+    };
+}
