a94125431d4a74311c9bca838f99f34e15d88839
Liam Nagels <Liam.Nagels@outlook.be>
2026-07-19 12:22:29 +0200

    Added proper error catching

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,55 +14,63 @@
 
 int main()
 {
-	Scythe::RendererAPI::SetAPI(Scythe::RendererAPI::API::OpenGL);
-	
-	Scythe::Window window(800, 600, "Scythe Engine");
-	Scythe::RendererAPI::Initialize();
-	
-	Scythe::Camera camera("MainCamera", glm::vec3(0.0f, 0.5f, 2.0f));
-
-	camera.SetPerspective(45.0f, 800.0f / 600.0f, 0.1f, 100.0f);
-
-	camera.LookAtRotation(glm::vec3(0.0f, 0.4f, 0.0f));
-
-	window.SetMainCamera(&camera);
-
-	spdlog::info("Working dir: {} ", std::filesystem::current_path().string());
-
-	auto shader = Scythe::Shader::Create("assets/shaders/basic.vert", "assets/shaders/basic.frag");
-	shader->Bind();
-
-	Scythe::Model bunny("assets/models/stanford-bunny.obj", "StanfordBunny", glm::vec3(0.0f, -0.085f, 0.0f));
-	bunny.SetScale(5.f);
-	spdlog::info("Model loaded");
-	
-	Scythe::RendererAPI::Get()->SetClearColor(glm::vec4(0.2f, 0.2f, 0.3f, 1.0f));
-	
-	float lastFrame = 0.0;
-	float bunnyYaw = 0.0f;
-	
-	while (!window.ShouldClose())
+	try 
 	{
-		float currentFrame = window.GetTime();
-		float deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
+		Scythe::RendererAPI::SetAPI(Scythe::RendererAPI::API::Vulkan);
+	
+		Scythe::Window window(800, 600, "Scythe Engine");
+		Scythe::RendererAPI::Initialize();
+	
+		Scythe::Camera camera("MainCamera", glm::vec3(0.0f, 0.5f, 2.0f));
 
-		Scythe::RendererAPI::Get()->Clear();
+		camera.SetPerspective(45.0f, 800.0f / 600.0f, 0.1f, 100.0f);
+
+		camera.LookAtRotation(glm::vec3(0.0f, 0.4f, 0.0f));
+
+		window.SetMainCamera(&camera);
+
+		spdlog::info("Working dir: {} ", std::filesystem::current_path().string());
+
+		auto shader = Scythe::Shader::Create("assets/shaders/basic.vert", "assets/shaders/basic.frag");
+		shader->Bind();
+
+		Scythe::Model bunny("assets/models/stanford-bunny.obj", "StanfordBunny", glm::vec3(0.0f, -0.085f, 0.0f));
+		bunny.SetScale(5.f);
+		spdlog::info("Model loaded");
+	
+		Scythe::RendererAPI::Get()->SetClearColor(glm::vec4(0.2f, 0.2f, 0.3f, 1.0f));
+	
+		float lastFrame = 0.0;
+		float bunnyYaw = 0.0f;
 		
-		bunnyYaw += 60.0f * deltaTime;
-		bunny.SetRotation(glm::vec3(0.0f, bunnyYaw, 0.0f));
+		while (!window.ShouldClose())
+		{
+			float currentFrame = window.GetTime();
+			float deltaTime = currentFrame - lastFrame;
+			lastFrame = currentFrame;
 
-		camera.Tick(deltaTime);
+			Scythe::RendererAPI::Get()->Clear();
 		
-		shader->SetMat4("uView", glm::value_ptr(window.GetMainCamera()->GetViewMatrix()));
-		shader->SetMat4("uProjection", glm::value_ptr(window.GetMainCamera()->GetProjectionMatrix()));
+			bunnyYaw += 60.0f * deltaTime;
+			bunny.SetRotation(glm::vec3(0.0f, bunnyYaw, 0.0f));
 
-		shader->SetMat4("uModel", glm::value_ptr(bunny.GetTransformMatrix()));
+			camera.Tick(deltaTime);
+		
+			shader->SetMat4("uView", glm::value_ptr(window.GetMainCamera()->GetViewMatrix()));
+			shader->SetMat4("uProjection", glm::value_ptr(window.GetMainCamera()->GetProjectionMatrix()));
 
-		bunny.Draw(shader);
+			shader->SetMat4("uModel", glm::value_ptr(bunny.GetTransformMatrix()));
 
-		window.SwapBuffers();
-		window.PollEvents();
+			bunny.Draw(shader);
+
+			window.SwapBuffers();
+			window.PollEvents();
+		}
+	}
+	catch (const std::exception& e)
+	{
+		spdlog::critical("Engine Initialization Failed: {}", e.what());
+		return -1;
 	}
 
 	return 0;
